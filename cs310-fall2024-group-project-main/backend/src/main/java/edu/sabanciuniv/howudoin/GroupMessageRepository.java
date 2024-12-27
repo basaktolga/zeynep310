2ba4c9d0c512f159a391d986eb658e8c31b4c003
Tolga Basak <tolgabasak1@hotmail.com>
2024-12-27 22:35:04 +0300

    Revert "push"
    This reverts commit b722ee72edb8fd25eaee20c6e1ccc5fd4883b678.

--- cs310-fall2024-group-project-main/backend/src/main/java/edu/sabanciuniv/howudoin/GroupMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.sabanciuniv.howudoin;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface GroupMessageRepository extends MongoRepository<GroupMessage, String> {
-    List<GroupMessage> findByGroupId(String groupId);
-}
--- cs310-fall2024-group-project-main/backend/src/main/java/edu/sabanciuniv/howudoin/GroupRepository.java
+++
@@ -1,6 +0,0 @@
-package edu.sabanciuniv.howudoin;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GroupRepository extends MongoRepository<Group, String> {
-}
